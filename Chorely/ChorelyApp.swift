6a55e0087d9a3b13dc9c5a07ecdd0166d8e58aaa
tianyudong <dongty05@gmail.com>
2025-10-29 16:26:29 -0700

    Initial Commit

---
+++ Chorely/ChorelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChorelyApp.swift
+//  Chorely
+//
+//  Created by Tian Yu Dong on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ChorelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chorely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chorely
+//
+//  Created by Tian Yu Dong on 10/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
