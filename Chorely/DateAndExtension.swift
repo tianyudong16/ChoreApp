e60078211559f2f248b127dce7787a31c19e2525
tianyudong16 <142982548+tianyudong16@users.noreply.github.com>
2025-11-08 17:18:25 -0800

    Add Date extension with various date utilities

---
+++ Chorely/DateAndExtension.swift
@@ -0,0 +1,81 @@
+//
+//  DateAndExtension.swift
+//  Chorely
+//
+//  Created by Tian Yu Dong on 11/8/25.
+//
+
+import Foundation
+
+extension Date {
+    static var capitalizedFirstLetterOfWeekdays: [String] {
+        let calendar = Calendar.current
+        let weekdays = calendar.weekdaySymbols
+        
+        return weekdays.map { weekday in
+            guard let firstLetter = weekday.first else { return "" }
+            return String(firstLetter).capitalized
+        }
+    }
+    
+    static var fullMonthNames: [String]
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        
+        return (1...12).compactMap { month in
+            dateFormatter.setLocalizedDateFormatFromTemplate("MMMM")
+            let date = Calendar.current.date(from: DateComponents(year: 2000, month: month, day: 1))
+            return date.map { dateFormatter.string(from: $0) }
+            
+        }
+    }
+    
+    var startOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.start
+    }
+    
+    var endOfMonth: Date {
+        let lastDay = Calendar.current.dateInterval(of: .month, for: self)!.end
+        return Calendar.current.date(byAdding: .day, value: -1, to: lastDay)!
+    }
+    
+    var startOfPreviousMonth: Date {
+        let dayInPreviousMonth = Calendar.current.date(byAdding: .month, value: -1, to: self)!
+        return dayInPreviousMonth.startOfMonth
+    }
+    
+    var numberOfDaysInMonth: Int {
+        Calendar.current.component(.day, from: endOfMonth)
+    }
+    
+    var sundayBeforeStart: Date {
+        let startOfMonthWeekday = Calendar.current.component(.weekday, from: startOfMonth)
+        let numberFromPreviousMonth = startOfMonthWeekday - 1
+        return Calendar.current.date(byAdding: .day, value: -numberFromPreviousMonth, to: startOfMonth)!
+    }
+    
+    var calendarDisplayDays: [Date] {
+        var days: [Date] = []
+        // Offset for current month days
+        for dayOffset in 0..<numberOfDaysInMonth {
+            let newDay = Calendar.current.date(byAdding: .day, value: dayOffset, to: startOfMonth)
+            days.append(newDay!)
+        }
+        // previous month days
+        for dayOffset in 0..<startOfPreviousMonth.numberOfDaysInMonth {
+            let newDay = Calendar.current.date(byAdding: .day, value: dayOffset, to: startOfPreviousMonth)
+            days.append(newDay!)
+        }
+        
+        return days.filter { $0 >= sundayBeforeStart && $0 <= endOfMonth }.sorted(by: <)
+    }
+    
+    var monthInt: Int {
+        Calendar.current.component(.month, from: self)
+    }
+    
+    var startOfDay: Date {
+        Calendar.current.startOfDay(for: self)
+    }
+}
