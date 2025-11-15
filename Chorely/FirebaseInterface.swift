a5d31e73e3d861e8a53d0a9aa113fdd718e07b65
BigBoiToasty <christopherle777@gmail.com>
2025-11-15 13:41:03 -0500

    addChores and getChores

--- Chorely/FirebaseInterface.swift
+++ Chorely/FirebaseInterface.swift
@@ -53,3 +53,45 @@ class FirebaseInterface {
         }
     }
 }
+
+func getChore(documentId:String, completion: @escaping ([String: Any]?) -> Void)
+{
+    db.collection("chores").document(documentId).getDocument{snapshot,
+        err in
+        if let err = err {
+            print("Error getting chore: \(err)")
+            completion(nil)
+            return
+        }
+        guard let data = snapshot?.data() else {
+            print("Chore not found")
+            completion(nil)
+            return
+        }
+        
+        completion(data)
+    }
+}
+func addChore(checklist: Bool, date: String, day: String, description: String, monthlyrepeatbydate: Bool, monthlyrepeatbyweek: String, Name: String, PriorityLevel: String, RepetitionTime: String, TimeLength: Int, assignedUsers:[String], completed: Bool, groupKey: Int){
+    db.collection("chores").addDocument(data: [
+        "Checklist": checklist,
+        "Date": date,
+        "Day": day,
+        "Description": description,
+        "MonthlyRepeatByDate": monthlyrepeatbydate,
+        "MonthlyRepeatByWeek": monthlyrepeatbyweek,
+        "Name": Name,
+        "PriorityLevel": PriorityLevel,
+        "RepetitionTime": RepetitionTime,
+        "TimeLength": TimeLength,
+        "assignedUsers":assignedUsers,
+        "completed": completed,
+        "groupKey": groupKey,
+    ]) { err in
+        if let err = err {
+            print("Error adding chore: \(err)")
+        } else {
+            print("Chore added successfully!")
+        }
+    }
+}
